9a9e98c0f0b8f6bd219975382a084bafa6ea8836
fencesitter1 <434716498@qq.com>
2024-01-28 22:25:01 +0800

    feat:add authlayout

--- src/_auth/forms/AuthLayout.tsx
+++ src/_auth/forms/AuthLayout.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import { Outlet, Navigate } from "react-router-dom";
 
-const AuthLayout = () => {
-  return <div>AuthLayout</div>;
-};
+export default function AuthLayout() {
+  const { isAuthenticated }: any = false;
 
-export default AuthLayout;
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+
+          <img
+            src="/assets/images/side-img.svg"
+            alt="logo"
+            className="hidden md:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
+        </>
+      )}
+    </>
+  );
+}
