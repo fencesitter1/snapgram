70576a1f0c18a7b39f995726ef0ca9e0a6b5e5e0
fencesitter1 <434716498@qq.com>
2024-01-29 21:45:31 +0800

    feat:add shared loader component

---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,7 @@
+export const Loader = () => {
+  return (
+    <div className="flex-center w-full">
+      <img src="/public/assets/icons/loader.svg" width={24} height={24}></img>
+    </div>
+  );
+};
